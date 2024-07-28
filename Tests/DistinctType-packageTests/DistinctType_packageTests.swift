ed0e1e2f85f98bc7a691be35be59053b81da0e1c
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2024-07-28 15:49:43 +0200

    Initial Commit

---
+++ Tests/DistinctType-packageTests/DistinctType_packageTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import DistinctType_package
+
+final class DistinctType_packageTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
